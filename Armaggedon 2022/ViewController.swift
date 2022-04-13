9050dd3c72de868bf502aee0b92496f253b7a35b
Victor Rubenko <vrubenko@gmail.com>
2022-04-14 02:13:24 +0300

    init

---
+++ Armaggedon 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Armaggedon 2022
+//
+//  Created by Victor Rubenko on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
